9a311def60cd47ac89a3463ed998cf57725b2768
Handser <dongdongliang13@hotmail.com>
2019-12-26 11:05:02 +0800

    Map集合 顺序测试

--- JavaTest/src/cn/test/JsonTest.java
+++ JavaTest/src/cn/test/JsonTest.java
@@ -1,15 +1,15 @@
 package cn.test;
 
 import cn.model.Demo;
-import net.sf.json.JSONObject;
+import com.alibaba.fastjson.JSONObject;
 
 public class JsonTest {
 
 	public static void main(String[] args) {
-		String str = "{\"sign\":\"sdfsaf\";\"id\":\"5\";\"name\":\"张三\"}";
-		String str1 = "{\"id\":\"5\";\"name\":\"张三\"}";
-		JSONObject jsonObject = JSONObject.fromObject(str);
-		Demo demo =(Demo)JSONObject.toBean(jsonObject, Demo.class);
+		String str = "{\"sign\":\"sdfsaf\",\"id\":\"5\",\"name\":\"张三\"}";
+		String str1 = "{\"id\":\"5\",\"name\":\"张三\"}";
+		JSONObject jsonObject = JSONObject.parseObject(str);
+		Demo demo =(Demo)JSONObject.toJavaObject(jsonObject, Demo.class);
 		System.out.println("demo = " + demo);
 	}
 }
---
+++ JavaTest/src/cn/test/URLEncodeTest.java
@@ -0,0 +1,20 @@
+package cn.test;
+
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+
+/**
+ * @author dongdongliang13@hotmail.com
+ * @date  
+ * @description application/x-wwww-form-urlencoded 格式 编码
+ **/
+public class URLEncodeTest {
+
+    public static void main(String[] args) throws Exception{
+        String str = "12346&= 测试 ";
+        String encodeStr = URLEncoder.encode(str, "utf-8");
+        System.out.println(encodeStr);
+        String decodeStr = URLDecoder.decode(encodeStr, "utf-8");
+        System.out.println(decodeStr);
+    }
+}
